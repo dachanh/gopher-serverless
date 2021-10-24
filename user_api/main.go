42dd47a5b3d0bfc9b484410302a702a75294025c
dachanh <ghostcodert7@gmail.com>
2021-10-25 01:57:29 +0700

    add user api

---
+++ user_api/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/dachanh/daita-serverless/user_api/model"
+	"net/http"
+)
+
+func main() {
+	lambda.Start(Handler)
+}
+
+func Handler(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	var user model.User
+	err := json.Unmarshal([]byte(req.Body), &user)
+	if err != nil {
+		return response("Couldn't unmarshal ", http.StatusBadRequest), nil
+	}
+	return response(user.UserName, http.StatusOK), nil
+}
+
+func response(body string, statusCode int) events.APIGatewayProxyResponse {
+	return events.APIGatewayProxyResponse{
+		StatusCode: statusCode,
+		Body:       string(body),
+		Headers: map[string]string{
+			"Access-Control-Allow-Origin": "*",
+		},
+	}
+}
---
+++ user_api/model/users.go
@@ -0,0 +1,8 @@
+package model
+
+type User struct {
+	ID       string `json:"id"`
+	UserName string `json:"username"`
+	Password string `json:"password"`
+	Email    string `json:"email"`
+}
